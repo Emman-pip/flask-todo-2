181066c581de088b7406ba486132cae40e0dd190
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-15 05:53:34 +0800

    Add delete button.

--- application/pages.py
+++ application/pages.py
@@ -2,16 +2,20 @@ from flask import Blueprint, render_template, request, current_app, redirect, ur
 # from application import Todo, db
 import application
 
+
 bp = Blueprint("pages", __name__)
+
 
 @bp.route("/")
 def home():
     return render_template('pages/home.html')
 
+
 @bp.route("/tasks")
 def tasks():
     tasks = application.Todo.query.all()
     return render_template('pages/tasks.html', tasks=tasks)
+
 
 @bp.route("/add-tasks", methods=("GET", "POST"))
 def addTasks():
@@ -27,3 +31,13 @@ def addTasks():
         
         print(request.form["task"])
     return render_template('pages/addTasks.html')
+
+@bp.route('/delete/<id>')
+def delete(id):
+    task = application.Todo.query.get_or_404(id)
+    try:
+        application.db.session.delete(task)
+        application.db.session.commit()
+        return redirect(url_for('pages.tasks'))
+    except:
+        return "There was an error deleting the task."
