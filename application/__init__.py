9ab23ddfbe4341a25081dc9a7bfea6cafedb06ef
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-15 19:39:55 +0800

    Change to online database and add a minor change.

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from application import pages
+import os
+from dotenv import load_dotenv
+from markupsafe import escape
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/todo'
+load_dotenv()
+# username = os.getenv['MYSQL_ADDON_USER']
+# password = os.getenv['MYSQL_ADDON_PASSWORD']
+# host = os.getenv['MYSQL_ADDON_HOST']
+# dbName = os.getenv['MYSQL_ADDON_DB']
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{escape(username)}:{escape(password)}@{escape(host)}/{escape(dbName)}'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_ADDON_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.register_blueprint(pages.bp)
 
